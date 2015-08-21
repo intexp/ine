ca0aefd7541f43c3eedad72de68899b1aaf4f907
Giorgi Bichinashvili <giorgi.bichinashvili@gmail.com>
2015-08-21 17:36:22 +0400

    Add and configure fos user bundle

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="ie_user", options={"collate"="utf8_general_ci", "charset"="utf8"})
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
